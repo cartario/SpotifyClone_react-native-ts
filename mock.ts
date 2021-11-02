78e81dd9a56355d777cc7bdc105b3f46846d5f87
VasiliyMac <cartario@yandex.ru>
2021-11-02 08:27:40 +0300

    getCategories with graphql

--- mock.ts
+++ mock.ts
@@ -66,3 +66,38 @@ export const albumsCategorysMock = [{
   }, 
 ]
 
+// "listAlbumCategorys
+// "items": [
+//   {
+//     "id": "bc7bcc78-30a9-4712-9669-d728afe632f1",
+//     "title": "Happy Tunes"
+//   },
+//   {
+//     "id": "1cbad856-19af-4970-b3f1-e51f9900de6f",
+//     "title": "Podcasts"
+//   },
+//   {
+//     "id": "0858551f-f3d1-4b96-9808-86b1c09df5c5",
+//     "title": "Lofi"
+//   },
+//   {
+//     "id": "7ca8d12a-a3c4-4232-85a9-97823ada793c",
+//     "title": "Happy Tunes"
+//   }
+// ]
+
+
+// "listAlbums": {
+//   "items": [
+//     {
+//       "id": "bfa68a76-51d1-4648-827f-7cfb4adc8b8d",
+//       "headline": "Eminem2",
+//       "albumCategoryId": "1cbad856-19af-4970-b3f1-e51f9900de6f"
+//     },
+//     {
+//       "id": "f4a4115a-be02-49e7-874a-1769905d7459",
+//       "headline": "Eminem1",
+//       "albumCategoryId": "bc7bcc78-30a9-4712-9669-d728afe632f1"
+//     }
+//   ]
+// }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button, ScrollView} from 'react-native';
 import {AlbumsCategory} from '../components';
 import {albumsCategorysMock} from '../mock';
 import {AlbumsCategoriesProps} from '../types';
+import {API, graphqlOperation} from 'aws-amplify';
+import {listAlbumCategorys} from '../src/graphql/queries';
 
 const HomeScreen = ({navigation}: AlbumsCategoriesProps) => {
+  useEffect(() => {
+    const fetchAlbumCategories = async () => {
+      try {
+        const {data} = await API.graphql(graphqlOperation(listAlbumCategorys));
+        console.log(data.listAlbumCategorys.items.map(each => each.title));
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchAlbumCategories();
+  }, []);
+
   return (
     <>
       <ScrollView>
